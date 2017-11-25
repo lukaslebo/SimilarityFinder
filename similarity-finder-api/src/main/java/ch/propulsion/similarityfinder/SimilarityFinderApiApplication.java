80a59f105e8f2af264f01509b6c07e1b60ffda04
lukas.lebo <lukas.lebo@hotmail.com>
2017-11-25 12:38:12 +0100

    spring starter project for backend

---
+++ similarity-finder-api/src/main/java/ch/propulsion/similarityfinder/SimilarityFinderApiApplication.java
@@ -0,0 +1,12 @@
+package ch.propulsion.similarityfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarityFinderApiApplication.class, args);
+	}
+}
---
+++ similarity-finder-api/src/test/java/ch/propulsion/similarityfinder/SimilarityFinderApiApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.propulsion.similarityfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimilarityFinderApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
