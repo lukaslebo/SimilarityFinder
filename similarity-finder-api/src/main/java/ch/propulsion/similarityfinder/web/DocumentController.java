5f9ac675ea581e1c449b14c9f77a5b252052203a
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-02 08:51:51 +0100

    document controller implemented

--- similarity-finder-api/src/main/java/ch/propulsion/similarityfinder/web/DocumentController.java
+++ similarity-finder-api/src/main/java/ch/propulsion/similarityfinder/web/DocumentController.java
@@ -1,9 +1,10 @@
 package ch.propulsion.similarityfinder.web;
 
-import java.util.ArrayList;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,25 +17,23 @@ import org.springframework.web.multipart.MultipartFile;
 
 import ch.propulsion.similarityfinder.domain.Document;
 import ch.propulsion.similarityfinder.domain.User;
-import ch.propulsion.similarityfinder.service.DocumentService;
-import ch.propulsion.similarityfinder.service.UserService;
+import ch.propulsion.similarityfinder.service.entity.UserService;
+import ch.propulsion.similarityfinder.service.parser.DocumentParser;
 import ch.propulsion.similarityfinder.service.storage.StorageService;
 
 @RestController
 @RequestMapping("/api/document")
 public class DocumentController {
 	
-	private final DocumentService documentService;
 	private final UserService userService;
 	private final StorageService storageService;
-	
-	private List<String> files = new ArrayList<>();
+	private final DocumentParser parser;
 	
 	@Autowired
-	public DocumentController(DocumentService documentService, UserService userService, StorageService storageService) {
-		this.documentService = documentService;
+	public DocumentController(UserService userService, StorageService storageService, DocumentParser parser) {
 		this.userService = userService;
 		this.storageService = storageService;
+		this.parser = parser;
 	}
 	
 	@GetMapping("/{userId}")
@@ -45,12 +44,12 @@ public class DocumentController {
 			response.put("user_exists", false);
 		}
 		else {
-			List<Document> resources = new ArrayList<Document>(user.getResources().values());
+			List<Document> resources = userService.getResources(userId);
 			resources.sort((a, b) -> a.getDateCreated().isBefore(b.getDateCreated()) ? -1 : 1);
 			response.put("user_exists", true);
 			response.put("id", user.getId());
 			response.put("expiresAt", user.getExpirationDate());
-			response.put("document", user.getDocument());
+			response.put("document", userService.getDocument(userId));
 			response.put("resources", resources);
 		}
 		return response;
@@ -58,31 +57,72 @@ public class DocumentController {
 	
 	@PostMapping("/{userId}/setDoc")
 	public Map<String, Object> setDocument(@PathVariable String userId, @RequestParam("file") MultipartFile file) {
+		Map<String, Object> response = new HashMap<>();
 		try {
 			storageService.store(file);
-			System.err.println("file stored successfully");
-			files.add(file.getOriginalFilename());
+			Path pathToFile = storageService.load(file.getOriginalFilename());
+			Document doc = parser.parseFileToDocument(pathToFile);
+			userService.setDocument(userId, doc);
+			userService.parseAll(userId);
+			response.put("status", "ok");
+			response.put("document", userService.getDocument(userId));
 		} catch (Exception e) {
 			System.err.println("Error in file upload!");
+			System.err.println(e);
+			e.printStackTrace(System.out);
+			response.put("status", "failed");
+			response.put("exception", e);
+		} finally {
+			this.storageService.deleteAll();
 		}
-		Map<String, Object> response = new HashMap<>();
-		response.put("text","test");
 		return response;
 	}
 	
 	@PostMapping("/{userId}/removeDoc")
 	public Map<String, Object> removeDocument(@PathVariable String userId) {
-		return null;
+		Map<String, Object> response = new HashMap<>();
+		userService.removeDocument(userId);
+		response.put("status", "ok");
+		response.put("document", userService.getDocument(userId));
+		return response;
 	}
 	
 	@PostMapping("/{userId}/addResource")
-	public Map<String, Object> addResource(@PathVariable String userId) {
-		return null;
+	public Map<String, Object> addResource(@PathVariable String userId, @RequestParam("file") MultipartFile[] files) {
+		Map<String, Object> response = new HashMap<>();
+		try {
+			for (MultipartFile file : files) {
+				storageService.store(file);				
+			}
+			Stream<Path> paths = storageService.loadAll();
+			List<Document> docs = parser.parseFilesToDocument(paths);
+			for (Document doc : docs) {
+				userService.addResource(userId, doc);
+			}
+			userService.parseAll(userId);
+			List<Document> resources = userService.getResources(userId);
+			resources.sort((a, b) -> a.getDateCreated().isBefore(b.getDateCreated()) ? -1 : 1);
+			response.put("status", "ok");
+			response.put("resources", userService.getResources(userId));
+		} catch (Exception e) {
+			System.err.println("Error in file upload!");
+			System.err.println(e);
+			e.printStackTrace(System.out);
+			response.put("status", "failed");
+			response.put("exception", e);
+		} finally {
+			this.storageService.deleteAll();
+		}
+		return response;
 	}
 	
-	@PostMapping("/{userId}/removeResource")
-	public Map<String, Object> removeResource(@PathVariable String userId) {
-		return null;
+	@PostMapping("/{userId}/removeResource/{resourceId}")
+	public Map<String, Object> removeResource(@PathVariable String userId, @PathVariable String resourceId) {
+		Map<String, Object> response = new HashMap<>();
+		userService.removeResource(userId, resourceId);
+		response.put("status", "ok");
+		response.put("resources", userService.getResources(userId));
+		return response;
 	}
 	
 }
