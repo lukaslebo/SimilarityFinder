deed3627d035047f18780bf6c9bf87d94b623a00
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-02 13:59:15 +0100

    text frame work in progress

--- similarity-finder-web/src/containers/TextFrame/index.js
+++ similarity-finder-web/src/containers/TextFrame/index.js
@@ -1,20 +1,98 @@
 import React from 'react';
 import './index.css';
+import { connect } from 'react-redux';
+
+// const dummyText = 'Pri eu clita reformidans, et vim autem justo eirmod. Pri ex facilis fuisset prodesset. Porro luptatum an nec, integre appetere facilisis ex nec, at mei case iudico molestiae. Ne iusto malorum eripuit eam. Euismod graecis evertitur vel in. Vim porro invenire no, pro at lorem probatus disputando. Qui idque ridens accommodare no, his alia decore eu. Et pri modo sensibus. Noster dolorum per cu, usu at quidam nominati. Ei quod wisi definitiones quo. Ad quot falli vel, duo in nihil philosophia concludaturque. Habeo postea referrentur mea te, vidisse suscipiantur ut vix, nam summo scripta definitiones ex. Qui inani imperdiet theophrastus id, solum philosophia has ut. Duis oporteat voluptaria cu per. Eam eu viris platonem dignissim, nisl persius civibus nam an. Maluisset concludaturque ne sea, eros brute ex vis, mel id oporteat sensibus. In ipsum tibique mea. An elit tritani ponderum has, ei alienum ponderum vituperatoribus usu, quo ne inani fuisset. Per quod vide detracto ei, pri in suscipit posidonium, ei vix virtute dignissim. Munere altera sanctus has ne, no pri deseruisse sadipscing. Te ius iracundia gubergren assueverit, quo ponderum conclusionemque ei, sea vero dolorum an. In enim dolores mei, vel at explicari corrumpit posidonium. Vim eu malis dolorem recteque, cu sea audire nominati. His eu assum libris molestie, eos te saepe meliore gloriatur. Nibh abhorreant duo no, no vero illum tollit his. Ad eos volumus lobortis, usu albucius neglegentur id, quo ut stet zril. Facilis torquatos vis no, nec quem dicit ad. Id reque labores his. Ipsum ridens mei ea, eros theophrastus ea cum. Vis ludus option sanctus cu. Ceteros convenire ut vis. Has id quis atqui omnium. Primis invenire cu sed, nec persius legendos ut. Ius civibus elaboraret id, mea eu graeco invidunt. Mucius invenire mea ne, appetere inciderint suscipiantur ei mel. Vim ad falli tamquam laboramus. Fabulas invenire te eam, mea ne cibo accusamus, alii munere eum et. Offendit eloquentiam mel te, sumo simul affert sea an, habemus denique ea cum. At vis hinc tibique, est id alii laudem admodum. Id nam ipsum elitr oblique, pri te nullam numquam. Labore saperet suscipit mei cu, elit assum tollit et has, nam falli maiestatis ex. Eum eleifend constituto eu, persecuti percipitur vel te. Sea ea fuisset omnesque argumentum. Has unum meliore ut. Duo minim interesset vituperatoribus ne, quo officiis sapientem te. Pri dicam ponderum consequat ex. Vix eu causae praesent. Mucius conclusionemque an usu, his an quot timeam appellantur. Has et illum impetus, ut per alii tritani, mazim tollit labitur in vim. Numquam pertinacia sea no, has ei quas detraxit. Fabulas scaevola principes ex usu, ne sed ullum putant maluisset. Mea ad iriure percipit electram, his et commodo denique platonem. Virtute oporteat repudiandae in duo, inani novum commune eu has. Affert dolorem pri et, nec ut verterem perfecto ullamcorper. Summo dicant iudicabit mel ne, placerat antiopam convenire sit id, partem vivendum no eum. Nec ea audiam sensibus reformidans, sit meis menandri ad. Putent officiis patrioque sit ei, eu ceteros fierent eloquentiam has. Qui nonumes habemus minimum id. Modus fabellas suavitate no est, cu mei movet laoreet. Mucius possim quaeque ea eum, ut pri partem populo aliquid. Repudiare persequeris at usu. Copiosae rationibus theophrastus at pri, ad maiestatis disputationi vix. Usu te facete oporteat. Te graece facilisi assueverit usu. Nec ullum placerat abhorreant ei. Duo ei eros nobis, ridens principes delicatissimi duo eu, veri bonorum eu nec. Facete senserit ut qui, pri ex augue ponderum. Et vix delenit pertinax signiferumque, eu dolorem ceteros tibique vix. Ex quodsi legendos vis. Brute nemore iriure id mel. An vel insolens theophrastus, eu stet ludus sit, cu sea postulant consulatu. Posse regione sit ne. Ut vis officiis mandamus praesent. Id eros dolores dissentiet vix, vel id prima persius volutpat, partem timeam ea eos. Erant quaeque pertinax an vix, no nihil volutpat mediocritatem eam, quem verear accusamus vix ex. Te quis principes eloquentiam nam. Odio omnes ea eum, et sea quaestio complectitur. Pri ea numquam nostrum evertitur, case assum primis te ius. Ius cu primis semper patrioque, in cum augue euripidis. Nisl adipisci ut per. Eu ius inani electram voluptatibus. At eam reque iudicabit temporibus. Usu no choro expetenda delicatissimi. Vim euismod scaevola te, sint vocent constituam qui ne. Eos ad tota euismod iudicabit, illud phaedrum accommodare id sed. Id nec facilisi perfecto, eu aperiri eligendi nam. Sit magna persequeris intellegebat at, in enim signiferumque est. Te vivendum principes comprehensam usu, ne usu graeco delenit accusamus, nemore impedit ad sed. Te vix dolorum suscipiantur. Posse praesent sea no, movet nostrud vel ut. Dicant conclusionemque eu usu, eos ornatus intellegat te. No justo vocent oporteat pro, his ex graeco detracto accusata. Aperiam nostrum tacimates sea ex. Petentium mediocritatem te mea, ne eruditi debitis intellegebat sea. Cu constituto referrentur mei, ad augue errem virtute has. Fuisset salutandi pri ad, cum ceteros nominavi scribentur te. Et lucilius vituperata definitiones est. Eum eu magna inani aliquip, wisi veri diceret ei mea. Eu doming constituam vix, sea ad harum placerat. Ad dolores hendrerit prodesset sed, te utamur deserunt sed. Eos eleifend complectitur cu, adolescens deseruisse ad eum, at debitis civibus sit. Aliquid appetere ut quo. Ne quando dicant equidem eum, et ius esse tempor aliquando, eu veniam iriure pro. Fugit scripta pertinax sed ut, ut iudico periculis vituperatoribus cum. Vim eu partem omnesque, ad vero accusamus vix, legere verear nam at. Ei eos soleat docendi cotidieque, tantas nostrum nec et. Et his molestie voluptaria, ut veri ancillae legendos vel. Mea id quas eirmod commune, vel tollit doming et, eam ad debet clita veritus. Munere dicunt vocibus mea te, in mutat menandri definiebas duo, cum et melius alienum urbanitas. Sea et invidunt assentior, partem possim volutpat id his. Nihil iracundia omittantur his ei, qui graeci mandamus voluptatibus ut, fugit deserunt theophrastus ex eam. Lobortis erroribus duo cu. An sit molestie adolescens adipiscing, alienum necessitatibus sea et. Ne has vitae ridens. Diam scriptorem appellantur his et, cu definitiones concludaturque ius. Vis omnis assueverit ne. Stet intellegat ea vix, quo rebum putent cu. At nostro accusata erroribus quo. No qui imperdiet euripidis. Cu est.';
 
 class TextFrame extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  // }
+  pd = (e) => {
+    e.preventDefault();
+  }
+
+  documentSelector = () => {
+    if (this.props.id === 'left') {
+      return this.props.document;
+    }
+    else if (this.props.id === 'right') {
+      if (this.props.resources.length === 0) {
+        return null;
+      }
+      return this.props.resources[this.props.resourceIndex];
+    }
+  }
+
+  defaultMessage = () => {
+    const leftMsg = <div className="default-msg">Add a document</div>;
+    const rightMsg = <div className="default-msg">Add one or more resources</div>;
+    if (this.props.id === 'left') {
+      return leftMsg;
+    }
+    else if (this.props.id === 'right') {
+      return rightMsg;
+    }
+  }
+
+  textFormatter = (doc) => {
+    if (this.props.similarities.length === 0) {
+      return doc.content;
+    }
+  }
+
+  displayText = () => {
+    const doc = this.documentSelector();
+    if (doc === null) {
+      return this.defaultMessage();
+    }
+    return this.textFormatter(doc);
+  }
+
+  getFilename = () => {
+    const doc = this.documentSelector();
+    if (doc === null) {
+      return "No file selected.";
+    }
+    return doc.fileName;
+  }
+
+  fileDisplay = () => {
+    const fileName = this.getFilename();
+    if (this.props.id === 'left') {
+      return <div className="btn btn-outline-primary file-display">{ fileName }</div>;
+    }
+    else if (this.props.id === 'right') {
+      return (
+        <div className="btn-group file-display">
+          <div className="btn btn-outline-primary btn-wide">{ fileName }</div>
+          <a href="" onClick={ this.pd } className="btn btn-outline-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span className="sr-only">Toggle Dropdown</span>
+          </a>
+          <div className="dropdown-menu btn-wide">
+            <a className="dropdown-item" href="" onClick={ this.pd }>other file</a>
+            <a className="dropdown-item" href="" onClick={ this.pd }>other file</a>
+            <a className="dropdown-item" href="" onClick={ this.pd }>other file</a>
+          </div>
+        </div>
+      );
+    }
+    return fileName;
+  }
 
   render() {
     return (
-      <div className="textframe">
-        Pri eu clita reformidans, et vim autem justo eirmod. Pri ex facilis fuisset prodesset. Porro luptatum an nec, integre appetere facilisis ex nec, at mei case iudico molestiae. Ne iusto malorum eripuit eam. Euismod graecis evertitur vel in. Vim porro invenire no, pro at lorem probatus disputando. Qui idque ridens accommodare no, his alia decore eu. Et pri modo sensibus. Noster dolorum per cu, usu at quidam nominati. Ei quod wisi definitiones quo. Ad quot falli vel, duo in nihil philosophia concludaturque. Habeo postea referrentur mea te, vidisse suscipiantur ut vix, nam summo scripta definitiones ex. Qui inani imperdiet theophrastus id, solum philosophia has ut. Duis oporteat voluptaria cu per. Eam eu viris platonem dignissim, nisl persius civibus nam an. Maluisset concludaturque ne sea, eros brute ex vis, mel id oporteat sensibus. In ipsum tibique mea. An elit tritani ponderum has, ei alienum ponderum vituperatoribus usu, quo ne inani fuisset. Per quod vide detracto ei, pri in suscipit posidonium, ei vix virtute dignissim. Munere altera sanctus has ne, no pri deseruisse sadipscing. Te ius iracundia gubergren assueverit, quo ponderum conclusionemque ei, sea vero dolorum an. In enim dolores mei, vel at explicari corrumpit posidonium. Vim eu malis dolorem recteque, cu sea audire nominati. His eu assum libris molestie, eos te saepe meliore gloriatur. Nibh abhorreant duo no, no vero illum tollit his. Ad eos volumus lobortis, usu albucius neglegentur id, quo ut stet zril. Facilis torquatos vis no, nec quem dicit ad. Id reque labores his. Ipsum ridens mei ea, eros theophrastus ea cum. Vis ludus option sanctus cu. Ceteros convenire ut vis. Has id quis atqui omnium. Primis invenire cu sed, nec persius legendos ut. Ius civibus elaboraret id, mea eu graeco invidunt. Mucius invenire mea ne, appetere inciderint suscipiantur ei mel. Vim ad falli tamquam laboramus. Fabulas invenire te eam, mea ne cibo accusamus, alii munere eum et. Offendit eloquentiam mel te, sumo simul affert sea an, habemus denique ea cum. At vis hinc tibique, est id alii laudem admodum. Id nam ipsum elitr oblique, pri te nullam numquam. Labore saperet suscipit mei cu, elit assum tollit et has, nam falli maiestatis ex. Eum eleifend constituto eu, persecuti percipitur vel te. Sea ea fuisset omnesque argumentum. Has unum meliore ut. Duo minim interesset vituperatoribus ne, quo officiis sapientem te. Pri dicam ponderum consequat ex. Vix eu causae praesent. Mucius conclusionemque an usu, his an quot timeam appellantur. Has et illum impetus, ut per alii tritani, mazim tollit labitur in vim. Numquam pertinacia sea no, has ei quas detraxit. Fabulas scaevola principes ex usu, ne sed ullum putant maluisset. Mea ad iriure percipit electram, his et commodo denique platonem. Virtute oporteat repudiandae in duo, inani novum commune eu has. Affert dolorem pri et, nec ut verterem perfecto ullamcorper. Summo dicant iudicabit mel ne, placerat antiopam convenire sit id, partem vivendum no eum. Nec ea audiam sensibus reformidans, sit meis menandri ad. Putent officiis patrioque sit ei, eu ceteros fierent eloquentiam has. Qui nonumes habemus minimum id. Modus fabellas suavitate no est, cu mei movet laoreet. Mucius possim quaeque ea eum, ut pri partem populo aliquid. Repudiare persequeris at usu. Copiosae rationibus theophrastus at pri, ad maiestatis disputationi vix. Usu te facete oporteat. Te graece facilisi assueverit usu. Nec ullum placerat abhorreant ei. Duo ei eros nobis, ridens principes delicatissimi duo eu, veri bonorum eu nec. Facete senserit ut qui, pri ex augue ponderum. Et vix delenit pertinax signiferumque, eu dolorem ceteros tibique vix. Ex quodsi legendos vis. Brute nemore iriure id mel. An vel insolens theophrastus, eu stet ludus sit, cu sea postulant consulatu. Posse regione sit ne. Ut vis officiis mandamus praesent. Id eros dolores dissentiet vix, vel id prima persius volutpat, partem timeam ea eos. Erant quaeque pertinax an vix, no nihil volutpat mediocritatem eam, quem verear accusamus vix ex. Te quis principes eloquentiam nam. Odio omnes ea eum, et sea quaestio complectitur. Pri ea numquam nostrum evertitur, case assum primis te ius. Ius cu primis semper patrioque, in cum augue euripidis. Nisl adipisci ut per. Eu ius inani electram voluptatibus. At eam reque iudicabit temporibus. Usu no choro expetenda delicatissimi. Vim euismod scaevola te, sint vocent constituam qui ne. Eos ad tota euismod iudicabit, illud phaedrum accommodare id sed. Id nec facilisi perfecto, eu aperiri eligendi nam. Sit magna persequeris intellegebat at, in enim signiferumque est. Te vivendum principes comprehensam usu, ne usu graeco delenit accusamus, nemore impedit ad sed. Te vix dolorum suscipiantur. Posse praesent sea no, movet nostrud vel ut. Dicant conclusionemque eu usu, eos ornatus intellegat te. No justo vocent oporteat pro, his ex graeco detracto accusata. Aperiam nostrum tacimates sea ex. Petentium mediocritatem te mea, ne eruditi debitis intellegebat sea. Cu constituto referrentur mei, ad augue errem virtute has. Fuisset salutandi pri ad, cum ceteros nominavi scribentur te. Et lucilius vituperata definitiones est. Eum eu magna inani aliquip, wisi veri diceret ei mea. Eu doming constituam vix, sea ad harum placerat. Ad dolores hendrerit prodesset sed, te utamur deserunt sed. Eos eleifend complectitur cu, adolescens deseruisse ad eum, at debitis civibus sit. Aliquid appetere ut quo. Ne quando dicant equidem eum, et ius esse tempor aliquando, eu veniam iriure pro. Fugit scripta pertinax sed ut, ut iudico periculis vituperatoribus cum. Vim eu partem omnesque, ad vero accusamus vix, legere verear nam at. Ei eos soleat docendi cotidieque, tantas nostrum nec et. Et his molestie voluptaria, ut veri ancillae legendos vel. Mea id quas eirmod commune, vel tollit doming et, eam ad debet clita veritus. Munere dicunt vocibus mea te, in mutat menandri definiebas duo, cum et melius alienum urbanitas. Sea et invidunt assentior, partem possim volutpat id his. Nihil iracundia omittantur his ei, qui graeci mandamus voluptatibus ut, fugit deserunt theophrastus ex eam. Lobortis erroribus duo cu. An sit molestie adolescens adipiscing, alienum necessitatibus sea et. Ne has vitae ridens. Diam scriptorem appellantur his et, cu definitiones concludaturque ius. Vis omnis assueverit ne. Stet intellegat ea vix, quo rebum putent cu. At nostro accusata erroribus quo. No qui imperdiet euripidis. Cu est.
+      <div className="frame-container">
+        { this.fileDisplay() }
+        <div className="textframe">
+          { this.displayText() }
+        </div>
       </div>
     );
   }
+
 }
 
+const mapStateToProps = (state) => ({
+  ...state.webApiReducer,
+});
 
-export default TextFrame;
+export default connect(mapStateToProps)(TextFrame);
