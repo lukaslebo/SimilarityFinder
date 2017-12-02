150c7e31f1729b76f096739bd983f77d31ef81b9
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-02 13:56:51 +0100

    remove button logic implemented

--- similarity-finder-web/src/containers/RemoveButton/index.js
+++ similarity-finder-web/src/containers/RemoveButton/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './index.css';
+import { connect } from 'react-redux';
+
+import { docRemove } from '../../store/actions';
 
 class RemoveButton extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  // }
+  remove = (e) => {
+    let frame = e.target.parentNode.parentNode.getAttribute('id');
+    let apiSuffix;
+    if (frame === 'leftframe') {
+      if (this.props.document === null) {
+        return;
+      }
+      apiSuffix = '/removeDocument';
+    }
+    else if (frame === 'rightframe') {
+      if (this.props.resources.length === 0) {
+        return;
+      }
+      apiSuffix = '/removeResource';
+    }
+    this.props.dispatch(docRemove(apiSuffix));
+  }
 
   render() {
     return (
       <div id={ this.props.id }>
-        <button className="btn btn-outline-primary remove-button float-right">Remove</button>
+        <button className="btn btn-outline-primary remove-button float-right" onClick={ this.remove }>Remove</button>
       </div>
     );
   }
+
 }
 
+const mapStateToProps = (state) => ({
+  ...state.webApiReducer,
+});
 
-export default RemoveButton;
+export default connect(mapStateToProps)(RemoveButton);
