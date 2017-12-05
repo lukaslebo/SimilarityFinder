bb2fd769cf5ea37ccf169f97300d55658155061b
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-05 15:12:52 +0100

    progress bar implementation - can be used for file upload

---
+++ similarity-finder-web/src/containers/ProgressBar/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './index.css';
+import { connect } from 'react-redux';
+
+class ProgressBar extends React.Component {
+
+  setProgress = (val) => {
+    if (val < 0 || val > 1 ) {
+      return;
+    }
+    const width = Math.round(val*100) + '%';
+    window.$('#bar').css('width', width);  
+  }
+
+  componentDidMount = () => {
+    this.setProgress(this.props.value);
+  }
+
+  componentDidUpdate = () => {
+    this.setProgress(this.props.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="seconds" class="seconds">
+          <div id="bar" class="bar"></div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+export default connect(mapStateToProps)(ProgressBar);
