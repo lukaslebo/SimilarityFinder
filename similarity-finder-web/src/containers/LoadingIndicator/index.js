b2f4ce18ea32f45e5a11b05a2a2479a20bcedf4c
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-05 11:40:11 +0100

    prevent null or undefined in loading text

--- similarity-finder-web/src/containers/LoadingIndicator/index.js
+++ similarity-finder-web/src/containers/LoadingIndicator/index.js
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 
 class LoadingIndicator extends React.Component {
   render() {
+    let value = '';
+    if (this.props.val >= 0) {
+      value = this.props.val + '%';
+    }
     return (
       <div className="dimmer">
         <div className="l-wrapper">
           
-          <div className="nonselectable spinner-progress">{ this.props.val }%</div>
+          <div className="nonselectable spinner-progress">{ value }%</div>
           <svg viewBox="-60 -60 240 240" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" xmlnsXlink="http://www.w3.org/1999/xlink">
             
             <symbol id="s--circle">
