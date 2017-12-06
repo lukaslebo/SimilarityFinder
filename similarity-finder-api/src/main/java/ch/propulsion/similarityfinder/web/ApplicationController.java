8104efd4be50803b761d4b8ba64fe944ac3ce6a1
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-06 18:47:19 +0100

    controls reworked

--- similarity-finder-api/src/main/java/ch/propulsion/similarityfinder/web/ApplicationController.java
+++ similarity-finder-api/src/main/java/ch/propulsion/similarityfinder/web/ApplicationController.java
@@ -9,11 +9,11 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.propulsion.similarityfinder.domain.User;
+import ch.propulsion.similarityfinder.service.detection.DocumentComparator;
 import ch.propulsion.similarityfinder.service.entity.UserService;
 
 @RestController
@@ -24,24 +24,58 @@ public class ApplicationController {
 	private final UserService userService;
 	private final TaskScheduler taskScheduler;
 	private final SimpMessagingTemplate template;
+	private final DocumentComparator comparator;
 
 	@Autowired
-	public ApplicationController(UserService userService, TaskScheduler taskScheduler, SimpMessagingTemplate template) {
+	public ApplicationController(UserService userService, TaskScheduler taskScheduler, SimpMessagingTemplate template, 
+			DocumentComparator comparator) {
 		this.userService = userService;
 		this.taskScheduler = taskScheduler;
 		this.template = template;
-	}
-	
-	// TODO: change to queue up to detection process
-	@SubscribeMapping("/{userId}")
-	public void subscribeTrigger(@DestinationVariable String userId) {
-		System.err.println("user with id: " + userId + "subscribed.");
+		this.comparator = comparator;
 	}
 	
 	@MessageMapping("/start-detection/{userId}")
 	@SendTo("/user-progress/{userId}")
 	public Map<String, Object> startDetectionProcess(@DestinationVariable String userId) throws InterruptedException {
-		System.err.println("startDetectionProcess called..." + userId);
+		Map<String, Object> response = new HashMap<>();
+		User user = userService.findById(userId);
+		if (user == null) {
+			response.put("status", "failed");
+			return response;
+		}
+		taskScheduler.scheduleWithFixedDelay(() -> {
+			System.out.println("Starting detection for user: " + userId);
+			comparator.initiateDetection(userId);
+		}, 0);
+		response.put("status","ok");
+		return response;
+	}
+	
+	public void updateProcessDone(String userId) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("status", "ok");
+		response.put("progress", 100);
+		response.put("isDone", true);
+		this.template.convertAndSend("/user-progress/"+userId, response);
+	}
+	
+	
+	public void updateProgressInfo(@DestinationVariable String userId, int progress) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("status", "ok");
+		response.put("progress", progress);
+		response.put("isDone", false);
+		this.template.convertAndSend("/user-progress/"+userId, response);
+	}
+	
+	/*
+	 *  Dummy Controls...
+	 */
+	
+	@MessageMapping("/start-dummy-detection/{userId}")
+	@SendTo("/user-progress/{userId}")
+	public Map<String, Object> startDummyDetectionProcess(@DestinationVariable String userId) throws InterruptedException {
 		Map<String, Object> response = new HashMap<>();
 		// initiate detection ...
 		User user = userService.findById(userId);
@@ -54,20 +88,13 @@ public class ApplicationController {
 		}
 		Thread.sleep(1000);
 		scheduledFuture = taskScheduler.scheduleAtFixedRate( () -> {
-			detectionProgress(userId);
+			dummyDetectionProgress(userId);
 		}, 30);
 		response.put("status","ok");
 		return response;
 	}
 	
-	
-	public void updateProgressInfo(@DestinationVariable String userId, int progress) {
-		Map<String, Object> response = new HashMap<>();
-		response.put("progress", progress);
-		this.template.convertAndSend("/user-progress/"+userId, response);
-	}
-	
-	private void detectionProgress(String userId) {
+	private void dummyDetectionProgress(String userId) {
 		if (dummyProgress == null) {
 			dummyProgress = 0.;
 		}
