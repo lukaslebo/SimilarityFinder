fce37cbacae7c7541852f9d9a8773bd3023a7a88
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-09 16:29:39 +0100

    logo in header

--- similarity-finder-web/src/containers/Header/index.js
+++ similarity-finder-web/src/containers/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './index.css';
+import Logo from './logo.js';
 
 class Header extends React.Component {
 
   render() {
     return (
       <div id={ this.props.id }>
-        <h3 className="blue-hover"><span>Similarity</span> Detection Tool</h3>
+        <h3 className="blue-hover"><Logo/><span>Similarity</span> Detection Tool</h3>
       </div>
     );
   }
