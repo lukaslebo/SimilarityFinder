67bc4c5db887275249757da2ef672516f07374aa
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-10 03:07:19 +0100

    mouse click jump to sim, mouse hover sim highlighting, prevent multiple recursive spans by component updating, fix case with multiple following linebreaks

--- similarity-finder-web/src/containers/HighlightedText/index.js
+++ similarity-finder-web/src/containers/HighlightedText/index.js
@@ -3,7 +3,16 @@ import './index.css';
 import { connect } from 'react-redux';
 import Rangy from 'rangy';
 
+import { selectResource } from '../../store/actions';
+
 class HighlightedText extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      docIsHighlighted: false,
+    };
+  }
 
   documentSelector = () => {
     if (this.props.id === 'left') {
@@ -18,14 +27,15 @@ class HighlightedText extends React.Component {
   }
 
   similaritySelector = () => {
+    const sim = JSON.parse(JSON.stringify(this.props.similarities));
     if (this.props.id === 'left') {
-      return this.props.similarities.map(el => el).sort( (a, b) => {
+      return sim.sort( (a, b) => {
         return a.startIndex < b.startIndex ? -1 : 1;
       });
     }
     else if (this.props.id === 'right') {
       const resourceId = this.props.resources[this.props.resourceIndex].id
-      return this.props.similarities.filter( el => {
+      return sim.filter( el => {
         return el.resourceId === resourceId;
       }).sort( (a, b) => {
         return a.resourceStartIndex < b.resourceStartIndex ? -1 : 1;
@@ -34,20 +44,57 @@ class HighlightedText extends React.Component {
   }
 
   adjustIndexes = (similarities) => {
+    let corr = 0;
     const doc = this.documentSelector();
     const words = doc.parsedDocument_punctuated;
     for (let sim of similarities) {
       if (this.props.id === 'left') {
         sim.startIndex = words.slice(0, sim.startIndex).join(" ").length;
-        sim.startIndex = sim.startIndex === 0 ? sim.startIndex : sim.startIndex + 1;
-        sim.endIndex = words.slice(0, sim.endIndex+1).join(" ").length;
+        while (true) {
+          let string = doc.content.substr(0, sim.startIndex+corr);
+          let matches = this.occurrences(string, "\n\n", true);
+          if (matches === corr) {
+            break;
+          }
+          corr = matches;
+        }
+        sim.startIndex = sim.startIndex === 0 ? sim.startIndex : sim.startIndex + 1 + corr;
+        sim.endIndex = words.slice(0, sim.endIndex+1).join(" ").length + corr;
       } else if (this.props.id === 'right') {
         sim.resourceStartIndex = words.slice(0, sim.resourceStartIndex).join(" ").length;
-        sim.resourceStartIndex = sim.resourceStartIndex === 0 ? sim.resourceStartIndex : sim.resourceStartIndex + 1;
-        sim.resourceEndIndex = words.slice(0, sim.resourceEndIndex+1).join(" ").length;
+        while (true) {
+          let string = doc.content.substr(0, sim.resourceStartIndex+corr);
+          let matches = this.occurrences(string, "\n\n", true);
+          if (matches === corr) {
+            break;
+          }
+          corr = matches;
+        }
+        sim.resourceStartIndex = sim.resourceStartIndex === 0 ? sim.resourceStartIndex + corr : sim.resourceStartIndex + 1 + corr;
+        sim.resourceEndIndex = words.slice(0, sim.resourceEndIndex+1).join(" ").length + corr;
       }
     }
   }
+
+  occurrences = (string, subString, allowOverlapping) => {
+    
+        string += "";
+        subString += "";
+        if (subString.length <= 0) return (string.length + 1);
+    
+        let n = 0,
+            pos = 0,
+            step = allowOverlapping ? 1 : subString.length;
+    
+        while (true) {
+            pos = string.indexOf(subString, pos);
+            if (pos >= 0) {
+                ++n;
+                pos += step;
+            } else break;
+        }
+        return n;
+    }
 
   setHighlighting = () => {
     let similarities = this.similaritySelector();
@@ -86,14 +133,68 @@ class HighlightedText extends React.Component {
   }
 
   clickSimilarity = (e) => {
-    console.log(e.target.getAttribute('data-sim-id'));
+    const simId = e.target.getAttribute('data-sim-id');
+    if (simId && this.props.id === 'left') {
+      const sim = this.similaritySelector().filter(el => el.id === simId)[0];
+      let resourceIndex;
+      this.props.resources.forEach( (el, index) => {
+        if (el.id === sim.resourceId) {
+          resourceIndex = index;
+        }}
+      )
+      setTimeout( () => {
+        const correspondingSpan = window.$(`div.text-container-right [data-sim-id=${simId}]`)[0];
+        const scrollbox = window.$('div.text-container-right')[0].parentElement;
+        this.scrollTo(scrollbox, correspondingSpan.offsetTop - 250, 500);
+      } , 0);
+      this.props.dispatch(selectResource(resourceIndex));
+    }
+    else if (simId && this.props.id === 'right') {
+      setTimeout( () => {
+        const correspondingSpan = window.$(`div.text-container-left [data-sim-id=${simId}]`)[0];
+        const scrollbox = window.$('div.text-container-left')[0].parentElement;
+        this.scrollTo(scrollbox, correspondingSpan.offsetTop - 250, 500);
+      } , 0);
+    }
+  }
+
+  scrollTo = (element, to, duration) => {
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+
+    const animateScroll = function(){
+        const easeInOutQuad = (t, s, c, d) => {
+            t /= d/2;
+            if (t < 1) {
+              return c/2*t*t + s;
+            }
+            t--;
+            return -c/2 * (t*(t-2) - 1) + s;
+        }     
+        currentTime += increment;
+        var val = easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    };
+    animateScroll();
   }
 
   componentDidMount = () => {
+    setupMouseHover();
     this.setHighlighting();
   }
 
   componentDidUpdate = () => {
+    if (this.props.id === 'left') {
+      const count = window.$('div.text-container-left').children().length;
+      if (count > 0) {
+        return;
+      }
+    }
     this.setHighlighting();
   }
 
@@ -112,3 +213,16 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(HighlightedText);
+
+const setupMouseHover = () => {
+  setInterval( () => {
+    window.$('.sim-active').removeClass('sim-active');
+    const hovered = window.$('.highlight:hover');
+    if (hovered.length > 0) {
+      const simId = hovered[hovered.length-1].getAttribute('data-sim-id');
+      const simPair = window.$(`[data-sim-id=${simId}]`);
+      simPair.addClass('sim-active');
+      simPair.find('*').addClass('sim-active');
+    }
+  }, 100);
+}
