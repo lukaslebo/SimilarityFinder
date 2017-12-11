a45206f461a0cf5259339e95fcfa9c6662382355
lukas.lebo <lukas.lebo@hotmail.com>
2017-12-11 05:04:05 +0100

    add cookies for storing userId - expires with expiration date | expiration date is refreshed on every action

--- similarity-finder-web/src/containers/WebApp/index.js
+++ similarity-finder-web/src/containers/WebApp/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import { connect } from 'react-redux';
+import Moment from 'moment';
 
 import Header from '../Header';
 import TextFrame from '../TextFrame';
@@ -12,23 +13,57 @@ import Footer from '../Footer';
 import UploadCard from '../UploadCard';
 import InfoCard from '../InfoCard';
 
-import { getNewUser, loadSpecificUser } from '../../store/actions';
+import { getNewUser, loadSpecificUser, refreshUser } from '../../store/actions';
 
 
 class WebApp extends React.Component {
 
-  componentDidMount = () => {
+  setCookie = (cname, cvalue, exmins) => {
+    var d = new Date();
+    d.setTime(d.getTime() + (exmins * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  getCookie = (cname) => {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+  }
+
+  setupUserId = () => {
     if (!this.props.isLoggedin) {
-      //this.props.dispatch(getNewUser());
-      const userId = '2419373b-8f09-4183-8c61-7d9bfb597543';
-      this.props.dispatch(loadSpecificUser(userId));
+      const userId = this.getCookie('userId');
+      if (userId !== '') {
+        this.props.dispatch(loadSpecificUser(userId));
+      }
+      else {
+        this.props.dispatch(getNewUser());
+      }
+    }
+    else {
+      if (this.props.expiresAt.diff(Moment.utc(), 'minutes', true) <= 175) {
+        this.props.dispatch(refreshUser());
+      }
+      this.setCookie('userId', this.props.userId, this.props.expiresAt.diff(new Moment(), 'minutes'));
     }
   }
 
+  componentDidMount = () => {
+    this.setupUserId();
+  }
+
   componentDidUpdate = () => {
-    if (!this.props.isLoggedin) {
-      this.props.dispatch(getNewUser());
-    }
+    this.setupUserId();
   }
 
   render() {
